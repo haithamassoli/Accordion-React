260af4fd1e2c7d8ebc81f58eb35b0d8f044cdfcc
haithamassoli <haitham.b.assoli@gmail.com>
2022-01-18 12:36:02 +0200

    Updates

---
+++ precache-manifest.1760d7524cdef9dfb2f083dcc92998ba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec5509b8decc3f8d666733e9aa9c95af",
+    "url": "/Accordion-React/index.html"
+  },
+  {
+    "revision": "4a999c3b96f0c8884bc1",
+    "url": "/Accordion-React/static/css/main.4b589e59.chunk.css"
+  },
+  {
+    "revision": "20fdb16bbef59bfa3076",
+    "url": "/Accordion-React/static/js/2.4750bdb4.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/Accordion-React/static/js/2.4750bdb4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a999c3b96f0c8884bc1",
+    "url": "/Accordion-React/static/js/main.9d09e3c8.chunk.js"
+  },
+  {
+    "revision": "aea0fd6f3de3ee8e0917",
+    "url": "/Accordion-React/static/js/runtime-main.fc9793ee.js"
+  }
+]);
